393e0ac02fd86e94fae0800a817e4c9db8d8bb9c
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-09-25 22:49:28 +0100

    NEW; PosNeg.java; solution to Warmup-1>posNeg

---
+++ src/PosNeg.java
@@ -0,0 +1,19 @@
+
+public class PosNeg {
+	public boolean posNeg(int a, int b, boolean negative) {
+		if (negative) {
+			return (a < 0 && b < 0);
+		} else {
+			if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+}
+
+/*
+ * else { return ((a < 0 && b > 0) || (a > 0 && b < 0)); }
+ */
