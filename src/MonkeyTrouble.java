91a7ea6dd06833c3e3c64cbdad68b0a016ca9a3c
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-09-25 19:48:21 +0100

    NEW; MonkeyTrouble.java; solution to Warmup-1>monkeyTrouble

---
+++ src/MonkeyTrouble.java
@@ -0,0 +1,14 @@
+
+public class MonkeyTrouble {
+
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+		if (aSmile && !bSmile) {
+			return false;
+		} else if (!aSmile && bSmile) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+}
