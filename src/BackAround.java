6907bc68979add468955726aa5e7c29c75925458
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-09-26 15:33:21 +0100

    NEW; BackAround.java; solution to Warmup-1>backAround

---
+++ src/BackAround.java
@@ -0,0 +1,9 @@
+
+public class BackAround {
+	public String backAround(String str) {
+		if (str.length() < 2)
+			return str + str + str;
+		else
+			return str.substring(str.length() - 1) + str + str.substring(str.length() - 1);
+	}
+}
