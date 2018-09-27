4bd4125b14dde66835060c9698e9b7f6a5448848
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-09-27 23:55:38 +0100

    NEW; StartOz.java; solution to Recursion-1>startOz

---
+++ src/Warmup_1/StartOz.java
@@ -0,0 +1,14 @@
+package Warmup_1;
+
+public class StartOz {
+	public String startOz(String str) {
+		String front = "";
+		if(str.length()>=1 && str.startsWith("o")) {
+			front = front + "o";
+		}
+		if(str.length()>=2 && str.charAt(1)=='z') {
+			front = front + "z";
+		}
+		return front;
+	}
+}
