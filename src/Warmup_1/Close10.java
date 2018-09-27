ae2e4875ce6b7e54d278fb823a325c3b35045967
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-09-28 00:04:09 +0100

    NEW; Close10.java; solution to Recursion-1>close10

---
+++ src/Warmup_1/Close10.java
@@ -0,0 +1,11 @@
+package Warmup_1;
+
+public class Close10 {
+	public int close10(int a, int b) {
+		  int aa;
+		  int bb;
+		  if(Math.abs(a-10)>Math.abs(b-10)) return b;
+		  else if(Math.abs(a-10)<Math.abs(b-10)) return a;
+		  else return 0;
+		}
+}
