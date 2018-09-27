0579ccd820402080f7f37b01ff2099f6d91ad73b
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-09-27 23:58:49 +0100

    NEW; IntMax.java; solution to Recursion-1>intMax

---
+++ src/Warmup_1/IntMax.java
@@ -0,0 +1,14 @@
+package Warmup_1;
+
+public class IntMax {
+	public int intMax(int a, int b, int c) {
+		int max;
+		if (a > b)
+			max = a;
+		else
+			max = b;
+		if (c > max)
+			max = c;
+		return max;
+	}
+}
