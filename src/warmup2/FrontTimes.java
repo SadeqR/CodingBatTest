bd8c33f20654636f6e922308299dc5e7aabc58c6
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-09-30 23:55:50 +0100

    NEW; FrontTimes.java; solution to warmup-2>frontTimes

---
+++ src/warmup2/FrontTimes.java
@@ -0,0 +1,14 @@
+package warmup2;
+
+public class FrontTimes {
+	public String frontTimes(String str, int n) {
+		String result = "";
+		if (str.length() < 3) {
+			for (int i = 0; i < n; i++)
+				result = result + str;
+		} else
+			for (int i = 0; i < n; i++)
+				result = result + str.substring(0, 3);
+		return result;
+	}
+}
