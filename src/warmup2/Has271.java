a723052ac02d6056fce82af28d97a3a878719dbc
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-10-04 22:29:14 +0100

    added; Has271.java; solution to Warmup-2>has271

---
+++ src/warmup2/Has271.java
@@ -0,0 +1,25 @@
+package warmup2;
+
+/*
+ * Given an array of ints, return true if it contains a 2, 7, 1 
+ * pattern: a value, followed by the value plus 5, followed by the 
+ * value minus 1. Additionally the 271 counts even if the "1" differs 
+ * by 2 or less from the correct value.
+ * 
+ * 
+ * has271([1, 2, 7, 1]) → true
+ * has271([1, 2, 8, 1]) → false
+ * has271([2, 7, 1]) → true
+ */
+
+public class Has271 {
+	public boolean has271(int[] nums) {
+		int x = 0;
+		for(int i =0; i<nums.length; i++) {
+			if(i+2==nums.length) break;
+			int diff = Math.abs(nums[i+2]-(nums[i]-1));
+			if(nums[i+1]==nums[i]+5 && diff <= 2) return true;
+		}
+		return false;
+	}
+}
