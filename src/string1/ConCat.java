57c259d2c710512641a7a6b73d4a1fb22ad7dd0c
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-10-07 00:16:18 +0100

    added; ConCat.java

---
+++ src/string1/ConCat.java
@@ -0,0 +1,19 @@
+package string1;
+
+/*
+Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+
+
+conCat("abc", "cat") → "abcat"
+conCat("dog", "cat") → "dogcat"
+conCat("abc", "") → "abc"
+*/
+
+public class ConCat {
+	public String conCat(String a, String b) {
+		if(b.length()>0 && a.length()>0 && a.charAt(a.length()-1)==b.charAt(0)) 
+			return a.substring(0, a.length()-1) + b.substring(0, b.length());
+		return a+b;
+	}
+
+}
