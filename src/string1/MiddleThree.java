8f021fc2802bff2623adc5c56bf2eed1a0d23238
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-10-06 23:37:46 +0100

    added; HasBad.java

--- src/string1/MiddleThree.java
+++ src/string1/MiddleThree.java
@@ -1,5 +1,15 @@
 package string1;
 
+/*
+Given a string of odd length, return the string length 3 from its 
+middle, so "Candy" yields "and". The string length will be at least 3.
+
+
+middleThree("Candy") → "and"
+middleThree("and") → "and"
+middleThree("solving") → "lvi"
+*/
+
 public class MiddleThree {
 	public String middleThree(String str) {
 		  return str.substring((((str.length()+1)/2)-2), (((str.length()+1)/2)+1));
