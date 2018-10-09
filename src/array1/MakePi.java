d42b0700405fd454eabc430a554190c77786468e
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-10-10 00:06:02 +0100

    added; MakePi.java

---
+++ src/array1/MakePi.java
@@ -0,0 +1,18 @@
+package array1;
+
+/*
+https://codingbat.com/prob/p167011
+
+Return an int array length 3 containing the first 3 digits of pi, 
+{3, 1, 4}.
+
+
+makePi() → [3, 1, 4]
+*/
+
+public class MakePi {
+	public int[] makePi() {
+		  int[] result = {3,4,1};
+		  return result;
+	}
+}
