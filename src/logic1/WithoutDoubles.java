cd36d737a866fa82510735a4655abc56243c69f3
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-10-22 14:38:07 +0100

    added; WithoutDoubles.java

---
+++ src/logic1/WithoutDoubles.java
@@ -0,0 +1,23 @@
+package logic1;
+/*
+https://codingbat.com/prob/p115233
+
+Return the sum of two 6-sided dice rolls, each in the range 1..6. 
+However, if noDoubles is true, if the two dice show the same value, 
+increment one die to the next value, wrapping around to 1 if its value 
+was 6.
+
+
+withoutDoubles(2, 3, true) → 5
+withoutDoubles(3, 3, true) → 7
+withoutDoubles(3, 3, false) → 6
+*/
+public class WithoutDoubles {
+	public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+		  if(noDoubles)
+			  if(die1==die2)
+				  if(die2==6) return die1 + 1;
+				  else return die1+die2+1;
+		  return die1+die2;
+	}
+}
