aede1cb445ff6bb9e57a1d8be003947c6e1c8f3a
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-10-25 20:23:46 +0100

    added; NoTeenSum.java

--- src/logic2/LuckySum.java
+++ src/logic2/LuckySum.java
@@ -14,6 +14,9 @@ luckySum(1, 13, 3) → 1
 */
 public class LuckySum {
 	public int luckySum(int a, int b, int c) {
-		  
+		  if(a==13) return 0;
+		  if(b==13) return a;
+		  if(c==13) return b+a;
+		  return a+b+c;
 	}
 }
