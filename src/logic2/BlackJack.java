fd8218cd3afb44a3abc9a7379f7a0d36e261901a
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-10-26 20:47:48 +0100

    added; BlackJack.java

---
+++ src/logic2/BlackJack.java
@@ -0,0 +1,22 @@
+package logic2;
+/*
+https://codingbat.com/prob/p117019
+
+Given 2 int values greater than 0, return whichever value is nearest to 
+21 without going over. Return 0 if they both go over.
+
+
+blackjack(19, 21) → 21
+blackjack(21, 19) → 21
+blackjack(19, 22) → 19
+*/
+
+public class BlackJack {
+	public int blackjack(int a, int b) {
+		  if(a>21 && b>21) return 0;
+		  else if(a>21) return b;
+		  else if(b>21) return a;
+		  if(Math.abs(b-21)>Math.abs(a-21)) return a;
+		  else return b;
+	}
+}
--- src/logic2/CloserFar.java
+++ src/logic2/CloserFar.java
@@ -14,8 +14,8 @@ closeFar(4, 1, 3) → true
 */
 public class CloserFar {
 	public boolean closeFar(int a, int b, int c) {
-		  if(Math.abs(a-b)<2 && Math.abs(a-c)>1) return true;
-		  if(Math.abs(a-c)<2 && Math.abs(a-b)>1) return true;
+		  if(Math.abs(a-b)<2 && Math.abs(a-c)>1 && Math.abs(b-c)>1) return true;
+		  if(Math.abs(a-c)<2 && Math.abs(a-b)>1 && Math.abs(b-c)>1) return true;
 		  return false;
 	}
 }
