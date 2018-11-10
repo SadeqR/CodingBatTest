df546f892d620cb1c32f58f453ee786052e1a685
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-11-10 20:31:18 +0000

    added; MakeChocolate.java

---
+++ src/logic2/MakeChocolate.java
@@ -0,0 +1,21 @@
+package logic2;
+/*
+https://codingbat.com/prob/p191363
+
+We want make a package of goal kilos of chocolate. We have small bars 
+(1 kilo each) and big bars (5 kilos each). Return the number of small 
+bars to use, assuming we always use big bars before small bars. 
+Return -1 if it can't be done.
+
+
+makeChocolate(4, 1, 9) → 4
+makeChocolate(4, 1, 10) → -1
+makeChocolate(4, 1, 7) → 2
+*/
+public class MakeChocolate {
+	public int makeChocolate(int small, int big, int goal) {
+		if(goal>small+(big*5)) return -1;  
+		else if((big*5)>=goal) return goal%5;
+		else return goal-big*5;
+	}
+}
