e5396e3cafa126a3da857f08f520240149fd790e
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-11-11 23:30:22 +0000

    added; SameStarChar.java solution not finished, does not meet all test cases yet

---
+++ src/string2/SameStarChar.java
@@ -0,0 +1,28 @@
+package string2;
+
+/*
+https://codingbat.com/prob/p194491
+
+Returns true if for every '*' (star) in the string, if there are chars 
+both immediately before and after the star, they are the same.
+
+
+sameStarChar("xy*yzz") → true
+sameStarChar("xy*zzz") → false
+sameStarChar("*xa*az") → true
+*/
+public class SameStarChar {
+	public boolean sameStarChar(String str) {
+		boolean match = true;
+		for (int i = 1; i < str.length(); i++) {
+			if (i > 0 && i <= str.length() - 2) {
+				if (str.charAt(i) == '*' && str.charAt(i - 1) == str.charAt(i + 1)) {
+					match = true;
+				} else {
+					match = false;
+				}
+			}
+		}
+		return match;
+	}
+}
