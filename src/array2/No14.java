29cd156ee34796125e75c884119511bf1bc84757
SadeqR <32779019+SadeqR@users.noreply.github.com>
2018-11-25 20:09:24 +0000

    added; No14.java

---
+++ src/array2/No14.java
@@ -0,0 +1,23 @@
+package array2;
+/*
+https://codingbat.com/prob/p136648
+
+Given an array of ints, return true if it contains no 1's or it 
+contains no 4's.
+
+
+no14([1, 2, 3]) → true
+no14([1, 2, 3, 4]) → false
+no14([2, 3, 4]) → true
+*/
+public class No14 {
+	public boolean no14(int[] nums) {
+		boolean no1 = false;
+		boolean no4 = false;
+		for(int i=0; i<nums.length; i++) {
+			if(nums[i]==1) no1 = true;
+			if(nums[i]==4) no4 = true;
+		}
+		return no1 || no4? false:true;
+	}
+}
