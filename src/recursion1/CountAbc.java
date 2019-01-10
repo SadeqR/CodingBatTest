95b3cabff52c13a7cda3fb55064dad3667ef0a35
SadeqR <32779019+SadeqR@users.noreply.github.com>
2019-01-10 13:49:53 +0000

    added; CountAbc.java

---
+++ src/recursion1/CountAbc.java
@@ -0,0 +1,21 @@
+package recursion1;
+/*
+https://codingbat.com/prob/p161124
+
+Count recursively the total number of "abc" and "aba" substrings that 
+appear in the given string.
+
+
+countAbc("abc") → 1
+countAbc("abcxxabc") → 2
+countAbc("abaxxaba") → 2
+*/
+public class CountAbc {
+	public int countAbc(String str) {
+		  if(str.length()<3) return 0;
+		  if(str.substring(0, 3).equals("abc") 
+				  || str.substring(0, 3).equals("aba"))
+			return 1 + countAbc(str.substring(2));
+		  else return countAbc(str.substring(1));
+	}
+}
