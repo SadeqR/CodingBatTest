cf0fd3fddbc89810fdcf545926bf68b0a6f06361
SadeqR <32779019+SadeqR@users.noreply.github.com>
2019-01-12 15:53:05 +0000

    added; StringClean.java

---
+++ src/recursion1/StringClean.java
@@ -0,0 +1,20 @@
+package recursion1;
+/*
+https://codingbat.com/prob/p104029
+
+Given a string, return recursively a "cleaned" string where adjacent 
+chars that are the same have been reduced to a single char. So "yyzzza" 
+yields "yza".
+
+
+stringClean("yyzzza") → "yza"
+stringClean("abbbcdd") → "abcd"
+stringClean("Hello") → "Helo"
+ */
+public class StringClean {
+	public String stringClean(String str) {
+		  if(str.length()<2) return str;
+		  if(str.charAt(0)==str.charAt(1)) return stringClean(str.substring(1, str.length()));
+		  else return str.charAt(0) + stringClean(str.substring(1, str.length()));
+	}
+}
