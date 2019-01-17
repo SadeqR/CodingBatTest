e03e4c8f225dbfc43cd106abf772fe04aec20fc0
SadeqR <32779019+SadeqR@users.noreply.github.com>
2019-01-17 19:56:39 +0000

    added; StrCopies.java

---
+++ src/recursion1/StrCopies.java
@@ -0,0 +1,25 @@
+package recursion1;
+/*
+https://codingbat.com/prob/p118182
+
+Given a string and a non-empty substring sub, compute recursively if at 
+least n copies of sub appear in the string somewhere, possibly with 
+overlapping. N will be non-negative.
+
+
+strCopies("catcowcat", "cat", 2) → true
+strCopies("catcowcat", "cow", 2) → false
+strCopies("catcowcat", "cow", 1) → true
+*/
+public class StrCopies {
+	public boolean strCopies(String str, String sub, int n) {
+		  if(str.length()<sub.length()) {
+		  if(n==0) return true;
+		  else return false;
+		  }
+		  if(str.subSequence(0, sub.length()).equals(sub)) { 
+			  return strCopies(str.substring(1), sub, --n);
+		  }
+		  return strCopies(str.substring(1), sub, n);
+	}
+}
