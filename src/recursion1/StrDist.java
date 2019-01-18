8091dc51e3e3ccbce5c9e7e2f8e3c97eb0d61030
SadeqR <32779019+SadeqR@users.noreply.github.com>
2019-01-18 10:39:29 +0000

    added; StrDist.java

---
+++ src/recursion1/StrDist.java
@@ -0,0 +1,45 @@
+package recursion1;
+/*
+https://codingbat.com/prob/p195413
+
+Given a string and a non-empty substring sub, compute recursively the 
+largest substring which starts and ends with sub and return its length.
+
+
+strDist("catcowcat", "cat") → 9
+strDist("catcowcat", "cow") → 3
+strDist("cccatcowcatxx", "cat") → 9
+*/
+public class StrDist {
+	public int strDist(String str, String sub) {
+		  if(str.length() < sub.length()) {
+			  return 0;
+		  }   
+		  else if(str.equals(sub)) {
+			  return str.length();
+		  } 
+		  else if(str.startsWith(sub)) {
+			  if(str.endsWith(sub)) return str.length();
+			  else return strDist(str.substring(0,str.length()-1), sub);
+		  }
+		  else return strDist(str.substring(1), sub);
+	}
+}
+
+/*
+public class StrDist {
+	public int strDist(String str, String sub) {
+		  if(str.startsWith(sub) && sub.endsWith(sub)) {
+			  return str.length();
+		  } 
+		  else if(str.length() < sub.length()) {
+			  return 0;
+		  }
+		  else if(str.startsWith(sub)) {
+			  if(str.endsWith(sub)) return str.length();
+			  else return strDist(str.substring(0,str.length()-1), sub);
+		  }
+		  else return strDist(str.substring(1), sub);
+	}
+}
+*/
